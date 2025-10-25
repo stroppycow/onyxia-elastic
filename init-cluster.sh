37ca7c64d56e177a80ff8c014eecd671395e863c
Théo Leroy <theo.leroy@insee.de>
2025-10-25 18:29:51 +0200

    Corecion script init

--- init-cluster.sh
+++ init-cluster.sh
@@ -1,3 +1,3 @@
 python3 -m pip install kubernetes pystache
 FOLDER=$(echo "$GIT_REPOSITORY" | awk -F'/' '{print $NF}' | cut -d'.' -f1)
-nohup python3 ./$FOLDER/init-cluster.py /home/onyxia/work/initialisation.log 2>&1 &
+nohup python3 ./work/$FOLDER/init-cluster.py > /home/onyxia/work/initialisation.log 2>&1 &
